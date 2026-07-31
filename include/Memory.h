7ed5254dd1eb7169d27dd59f693d9cbaef8b6b16
InfinityHty <infinityhty.sjtu.edu.cn>
2026-07-31 11:35:33 +0800

    Fix a BIG bug.

--- include/Memory.h
+++ include/Memory.h
@@ -6,27 +6,28 @@
 #include <cstdint>
 #include <iostream>
 #include "Decoder.h"
-const int MAXN = 1e6 + 5;
+const int MAXN = 2e6 + 5;
 class Memory {
 public:
     Memory() {
         for (int i = 0; i < MAXN; i++) {
-            instructions[i] = 0;
-            data[i] = 0;
+            mem[i] = 0;
         }
     }
     void Load();
     uint8_t GetBinaryIns(const uint32_t addr) const {
-        return instructions[addr];
+        return mem[addr];
     }
     uint32_t AccessData(InsType type,uint32_t cont,uint32_t addr);
 private:
-    uint8_t instructions[MAXN]{}; // 指令
-    uint8_t data[MAXN]{}; // 数据
+    // uint8_t instructions[MAXN]{}; // 指令
+    // uint8_t data[MAXN]{}; // 数据
+    uint8_t mem[MAXN];
     static int AddressToIndex(const char *addr) {
         int index = 0,digit = 0;
         for (int i = 7; i >= 0; i--) {
-            index += (addr[i] - '0') * (1 << (digit * 4));
+            if ('0' <= addr[i] && addr[i] <= '9') index += (addr[i] - '0') * (1 << (digit * 4));
+            else index += (addr[i] - 'A' + 10) * (1 << (digit * 4));
             digit++;
         }
         return index;
--- src/Memory.cpp
+++ src/Memory.cpp
@@ -16,10 +16,10 @@ void Memory::Load() {
             int i = 0;
             while (line[i] != '\0') {
                 // Read 1 byte
-                if (line[i] >= '0' && line[i] <= '9') instructions[cur_add] = (line[i] - '0') * 16;
-                else instructions[cur_add] = (line[i] - 'A' + 10) * 16;
-                if (line[i + 1] >= '0' && line[i + 1] <= '9') instructions[cur_add] += line[i + 1] - '0';
-                else instructions[cur_add] += line[i + 1] - 'A' + 10;
+                if (line[i] >= '0' && line[i] <= '9') mem[cur_add] = (line[i] - '0') * 16;
+                else mem[cur_add] = (line[i] - 'A' + 10) * 16;
+                if (line[i + 1] >= '0' && line[i + 1] <= '9') mem[cur_add] += line[i + 1] - '0';
+                else mem[cur_add] += line[i + 1] - 'A' + 10;
                 cur_add++;
                 i += 2;
                 if (line[i] == ' ') i++;
@@ -31,31 +31,31 @@ uint32_t Memory::AccessData(InsType type,uint32_t cont,uint32_t addr) {
     // Load
     // 小端序
     if (type == lb) {
-        return static_cast<int>(data[addr] << 24) >> 24;
+        return static_cast<int>(mem[addr] << 24) >> 24;
     }
-    if (type == lbu) return data[addr];
+    if (type == lbu) return mem[addr];
     if (type == lh) {
-        return static_cast<int>((data[addr] + (data[addr + 1] << 8)) << 16) >> 16;
+        return static_cast<int>((mem[addr] + (mem[addr + 1] << 8)) << 16) >> 16;
     }
-    if (type == lhu) return data[addr] + (data[addr + 1] << 8);
+    if (type == lhu) return mem[addr] + (mem[addr + 1] << 8);
     if (type == lw) {
         // std::cerr << "addr: " << addr << '\n';
-        return data[addr] + (data[addr + 1] << 8) + (data[addr + 2] << 16) + (data[addr + 3] << 24);
+        return mem[addr] + (mem[addr + 1] << 8) + (mem[addr + 2] << 16) + (mem[addr + 3] << 24);
     }
     // Store
     if (type == sb) {
-        data[addr] = cont & 0xFF;
+        mem[addr] = cont & 0xFF;
     }
     if (type == sh) {
-        data[addr] = cont & 0xFF;
-        data[addr + 1] = (cont >> 8) & 0xFF;
+        mem[addr] = cont & 0xFF;
+        mem[addr + 1] = (cont >> 8) & 0xFF;
     }
     if (type == sw) {
         // std::cerr << "cont: " << cont << "addr: " << addr << '\n';
-        data[addr] = cont & 0xFF;
-        data[addr + 1] = (cont >> 8) & 0xFF;
-        data[addr + 2] = (cont >> 16) & 0xFF;
-        data[addr + 3] = (cont >> 24) & 0xFF;
+        mem[addr] = cont & 0xFF;
+        mem[addr + 1] = (cont >> 8) & 0xFF;
+        mem[addr + 2] = (cont >> 16) & 0xFF;
+        mem[addr + 3] = (cont >> 24) & 0xFF;
     }
     // default
     return 0;
